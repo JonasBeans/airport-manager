bf79bd3639b6f95bb0718a422f720d430b256df3
JonasBeans <boon.jonas.git@outlook.com>
2022-11-20 22:39:24 +0100

    Flight: Get and Create with pilot

---
+++ src/main/resources/db/changelog/migrations/4-create-flight.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS application.flight
+(
+    id varchar(6) NOT NULL,
+    pilot_id integer NOT NULL,
+    CONSTRAINT flight_pkey PRIMARY KEY (id),
+    CONSTRAINT fk_pilot_id FOREIGN KEY (pilot_id)
+        REFERENCES application.pilot (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+INSERT INTO application.flight(
+	id, pilot_id)
+	VALUES ('QTA543', 1);
